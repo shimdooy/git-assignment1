745969f1dc2f602b4a6f11751c656d140e94eb37
Doo <shimdooy@grinnell.edu>
2014-04-01 20:17:04 -0500

    I changed the name of the procedure

--- utils.rkt
+++ utils.rkt
@@ -10,10 +10,10 @@
 ;;;   list-of-nums, a list of integers
 ;;; Preconditions:
 ;;;   [No additional]
-;;; Postconditions:
+;;; Postconditions: 
 ;;;   (length list-of-nums) == n
 ;;;   (list-ref list-of-nums i) = i+1
 ;;;     for all i, 0 <= i < n.
-(define nums
+(define first-n-numbers
   (lambda (n)
     (cdr (iota (+ n 1)))))
